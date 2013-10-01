f51cc03fcebcf9259b4dbf625580bb75da9c6bef
Ross J. Reedstrom <reedstrm@cnx.org>
2013-10-01 14:31:44 -0500

    only trigger on index.cnxml

--- schema/schema.sql
+++ schema/schema.sql
@@ -287,9 +287,8 @@ AS $$
 $$ LANGUAGE plpythonu;
 
 CREATE TRIGGER module_file_added
-  AFTER INSERT ON module_files FOR EACH ROW
+  AFTER INSERT ON module_files FOR EACH ROW WHEN ( new.filename = 'index.cnxml' )
   EXECUTE PROCEDURE add_module_file();
-
 
 
 
