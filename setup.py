721cac4bc9a67dd770391916383f7ff38d4b87cb
Michael Mulich <michael.mulich@gmail.com>
2016-03-24 09:50:16 -0700

    Use py.test for testing

--- setup.py
+++ setup.py
@@ -5,10 +5,14 @@ from setuptools import setup, find_packages
 
 IS_PY3 = sys.version_info > (3,)
 
+setup_requires = (
+    'pytest-runner',
+    )
 install_requires = (
     'psycopg2',
     )
 tests_require = [
+    'pytest',
     ]
 extras_require = {
     'test': tests_require,
@@ -26,6 +30,7 @@ setup(
     url="https://github.com/connexions/cnx-db",
     license='LGPL, See also LICENSE.txt',
     description=description,
+    setup_requires=setup_requires,
     install_requires=install_requires,
     tests_require=tests_require,
     extras_require=extras_require,
