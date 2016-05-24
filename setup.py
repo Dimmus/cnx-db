0b1a17b2bca04d60ce4514fe83640abdf729ab05
Michael Mulich <michael.mulich@gmail.com>
2016-05-24 12:20:34 -0700

    Ensure package data is included in the distribution

--- setup.py
+++ setup.py
@@ -46,6 +46,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
+        'cnxdb': ['*-sql/*.sql', '*-sql/**/*.sql'],
+        'cnxdb.tests': ['data/init/**/*.*'],
         },
     entry_points="""\
     """,
