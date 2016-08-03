5a872a93fe97ddc5c976c08f278bc0334b88bad8
Michael Mulich <michael.mulich@gmail.com>
2016-08-03 09:23:49 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if not IS_PY3:
 
 setup(
     name='cnx-db',
-    version='0.2.0',
+    version='0.2.1',
     author='Connexions team',
     author_email='info@cnx.org',
     url="https://github.com/connexions/cnx-db",
