74cc87393d53d2f4828208bb31a969d89a3b3d9e
Michael Mulich <michael.mulich@gmail.com>
2017-03-16 20:20:36 -0700

    Set up versioneer for this projects

--- cnxdb/__init__.py
+++ cnxdb/__init__.py
@@ -4,3 +4,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 migrations = os.path.join(here, 'migrations')
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
